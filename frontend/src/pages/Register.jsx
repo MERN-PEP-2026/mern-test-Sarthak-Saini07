57386c326ddec9b070dc09a3703cb8ab877fd4ef
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-27 14:57:43 +0530

    register page added

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import API from "../api";
+
+function Register() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await API.post("/auth/register", form);
+    alert("Registered Successfully");
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Name"
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
+        <br />
+        <input
+          placeholder="Email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+        <br />
+        <button type="submit">Register</button>
+      </form>
+
+      <p>
+        Already have account? <Link to="/login">Login</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Register;
