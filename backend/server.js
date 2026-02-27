2cc150e63f4aacc8151fd2710fb857b2f5f913fe
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-27 09:54:42 +0530

    basic server added

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
