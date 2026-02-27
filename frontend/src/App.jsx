ca7fe3170c26a174ff288e1fc1997a06bf046d88
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-27 14:55:46 +0530

    frontend vite setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import {Routes, Route} from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
