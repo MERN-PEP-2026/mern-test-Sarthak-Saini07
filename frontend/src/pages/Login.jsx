d09f6e6ae194c323262353c3b6d169a8d676ae2e
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-27 14:58:21 +0530

    login page added

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import API from "../api";
+
+function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await API.post("/auth/login", form);
+    localStorage.setItem("token", data.token);
+    navigate("/dashboard");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+
+      <p>
+        New user? <Link to="/">Register</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
