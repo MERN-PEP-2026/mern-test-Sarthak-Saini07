e4768a28ff057743c25c9e4b81a8537689757aae
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-27 10:32:22 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,3 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+    res.send("Server running");
+})
+app.listen(5000,()=>{
+    console.log("server is running at port 5000");
+})
